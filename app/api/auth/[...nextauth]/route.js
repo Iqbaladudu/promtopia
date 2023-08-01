43e9ae7c103a31b6d96956024921a36bd5901dd3
Muhammad Iqbal <66395282+Iqbaladudu@users.noreply.github.com>
2023-08-01 17:07:53 +0300

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,6 +3,8 @@ import GoogleProvider from 'next-auth/providers/google';
 import { connectToDB } from '@utils/database';
 import User from '@models/user';
 
+export const runtime = 'edge';
+
 // Establish database connection
 connectToDB();
 
