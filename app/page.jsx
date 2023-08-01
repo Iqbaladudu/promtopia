b43a252d85f65bc03018ea2419f62e7d08343f05
Muhammad Iqbal <66395282+Iqbaladudu@users.noreply.github.com>
2023-08-01 17:02:35 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,5 @@
 import Feed from "@components/Feed"
 
-export const runtime = 'edge' // 'nodejs' (default) | 'edge'
-
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
